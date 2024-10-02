2441b2d4287441e663f531ec52943b22159b3e57
Gregua1232 <grishaseleznev@yandex.ru>
2024-10-02 19:18:08 +0300

    .

--- Assets/Scripts/Player/Player Inventory.cs
+++ Assets/Scripts/Player/Player Inventory.cs
@@ -2,22 +2,22 @@ using System;
 
 public class PlayerInventory {
 
-    private Weapon _equipedWeapon;
-    public Weapon EquipedWeapon {
-        get {
-            return _equipedWeapon;
-        }
-        private set {
-            _equipedWeapon = value;
-        }
-    }
+    // private Weapon _equipedWeapon;
+    // public Weapon EquipedWeapon {
+    //     get {
+    //         return _equipedWeapon;
+    //     }
+    //     private set {
+    //         _equipedWeapon = value;
+    //     }
+    // }
     private Item[] holdsItems;
 
     private static PlayerInventory instance;
 
     private PlayerInventory()
     {
-        EquipedWeapon = new MeleeWeapon();
+        // EquipedWeapon = new MeleeWeapon();
     }
 
     public static PlayerInventory getInstance() {
