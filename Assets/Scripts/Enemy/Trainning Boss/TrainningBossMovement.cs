b86ac6daf7f346ad62cf443509bc94a853f5c4db
Gregua1232 <grishaseleznev@yandex.ru>
2025-03-05 21:52:39 +0300

    Добавил таксебе эффекты при атаке на платформу боссу

--- Assets/Scripts/Enemy/Trainning Boss/TrainningBossMovement.cs
+++ Assets/Scripts/Enemy/Trainning Boss/TrainningBossMovement.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class TrainningBossMovement : MonoBehaviour
@@ -8,11 +11,12 @@ public class TrainningBossMovement : MonoBehaviour
     public List<GameObject> platforms; // Массив платформ, на которые может прыгать босс
     public Rigidbody2D platformRigid;
     private int currentPlatform = -1;
-    public float jumpForce = 10f; // Сила прыжка
-    public float groundPoundRadius = 4f; // Радиус урона при ударе по земле
     public int groundPoundDamage = 10; // Урон от удара по земле
+    public Transform attackTransform;
     public GameObject projectilePrefab; // Префаб стрелы
-    public float projectileSpeed = 10f; // Скорость стрелы
+    public GroundProjectiles leftProj;
+    public GroundProjectiles righttProj;
+    public float projectileSpeed = 30f; // Скорость стрелы
     public float attackCooldown = 2f; // Время между атаками
     public float meleeRange = 1.5f; // Дистанция для ближней атаки
     public int meleeDamage = 5; // Урон от ближней атаки
@@ -28,50 +32,46 @@ public class TrainningBossMovement : MonoBehaviour
     private BoxCollider2D boxCollider;
     private Animator animator;
     private float lastAttackTime;
-    private LineRenderer lineRenderer;
-    public int resolution = 20; // Количество точек для отрисовки
-
-    private Vector2 start;
-    private Vector2 mid;
-    private Vector2 end;
+    private bool isJumpingAttack = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
         animator = GetComponent<Animator>();
-        // player = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         lastAttackTime = -attackCooldown; // Чтобы босс мог атаковать сразу
         
-        lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.positionCount = resolution;
     }
 
     void Update()
     {
         if (Time.time - lastAttackTime >= attackCooldown)
         {
-            DecideAttack();
             lastAttackTime = Time.time;
         }
+        DecideAttack();
+        TurnToPlayer();
     }
 
     private void DecideAttack() {
-        var random = new System.Random();
-        currentPlatform = random.Next(platforms.Count);
-        JumpToPlatform();
+        // if (UserInput.instance.controls.Jumping.Jump.WasPressedThisFrame()) {
+        //     var random = new System.Random();
+        //     currentPlatform = random.Next(platforms.Count);
+        //     JumpToPlatform();
+        // }
+        // if (UserInput.instance.controls.GameInteraction.Interact.WasPressedThisFrame()) {
+        //     GroundPound();
+        // }
     }
 
     #region Movement methods
 
-    void JumpToPlatform()
+    private void JumpToPlatform()
     {
         if (!isGrounded()) {
             return;
         }
-        
-        currentPlatform++;
-        currentPlatform %= platforms.Count();
 
         Vector2 startPos = transform.position;
         Vector2 endPos = platforms[currentPlatform].transform.position;
@@ -79,10 +79,6 @@ public class TrainningBossMovement : MonoBehaviour
         Vector2 point1 = startPos;
         Vector2 point2 = endPos;
         Vector2 point3 = midPos;
-
-        start = startPos;
-        mid = midPos;
-        end = endPos;
         
         float x1 = startPos.x, y1 = startPos.y;
         float x2 = endPos.x, y2 = endPos.y;
@@ -103,10 +99,11 @@ public class TrainningBossMovement : MonoBehaviour
             return;
         }
         
+        isJumpingAttack = true;
+        
         // Находим вершину параболы
         float h = -b / (2 * a);
         float k = c - (b * b) / (4 * a);
-        mid = new Vector2(h, k);
 
         // Рассчитываем время полета (предполагаем, что начальная и конечная точки — point1 и point3)
         
@@ -129,63 +126,29 @@ public class TrainningBossMovement : MonoBehaviour
         // Применяем скорость к Rigidbody2D
         rb.velocity = initialVelocity;
 
-        
-
         // Анимация прыжка
         // animator.SetTrigger("Jump");
     }
 
-    void MeleeAttack()
+    private void MeleeAttack()
     {
         // Анимация ближней атаки
         // animator.SetTrigger("MeleeAttack");
 
-        // Проверяем, находится ли игрок в радиусе атаки
-        if (Vector2.Distance(transform.position, player.position) <= meleeRange)
-        {
-            player.GetComponent<PlayerHealth>().TakeDamage(meleeDamage);
-        }
+        // TODO: Сделать анимацию на каждый фрейм анимации сделать метод который будет наносить урон если в радиусе атаки оказался игрок
+        
     }
 
-    void GroundPound()
-    {
+    private void GroundPound() {
+        isJumpingAttack = false;
+
         // Анимация удара по земле
         // animator.SetTrigger("GroundPound");
 
-        // Определяем область под боссом
-        Vector2 raycastOrigin = transform.position; // Начальная точка для Raycast
-        float raycastDistance = groundPoundRadius; // Дистанция для проверки
-        Vector2 directionL = Vector2.left; // Направление влево
-        Vector2 directionR = Vector2.right; // Направление вправо
-
-        // Используем Raycast для проверки попадания
-        // RaycastHit2D[] hits = Physics2D.RaycastAll(raycastOrigin, direction, raycastDistance);
-
-        // // Перебираем все объекты, которые попали под луч
-        // foreach (RaycastHit2D hit in hits)
-        // {
-        //     if (hit.collider.CompareTag("Player"))
-        //     {
-        //         // Наносим урон игроку
-        //         hit.collider.GetComponent<PlayerHealth>().TakeDamage(groundPoundDamage);
-        //     }
-        // }
-
-        // Альтернативно, можно использовать BoxCast для более точной проверки области
-        
-        Vector2 boxSize = new Vector2(groundPoundRadius * 2, 1f); // Размер области проверки
-        // RaycastHit2D[] boxHits = Physics2D.BoxCastAll(raycastOrigin, boxSize, 0f, direction, raycastDistance);
-
-        // foreach (RaycastHit2D hit in boxHits)
-        // {
-        //     if (hit.collider.CompareTag("Player"))
-        //     {
-        //         hit.collider.GetComponent<PlayerHealth>().TakeDamage(groundPoundDamage);
-        //     }
-        // }
+        EnableFroundProjectiles();
     }
 
-    void ShootProjectile()
+    private void ShootProjectile()
     {
         GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
         Vector2 direction = (player.position - transform.position).normalized;
@@ -195,10 +158,21 @@ public class TrainningBossMovement : MonoBehaviour
         // animator.SetTrigger("Shoot");
     }
 
+    private void TurnToPlayer() {
+        var direction = -Mathf.Sign(transform.position.x - player.position.x);
+        if ( direction != Mathf.Sign(transform.localScale.x) ) {
+            Flip();
+        }
+    }
+
     #endregion
 
     #region help methods
-    // Метод для решения системы линейных уравнений
+    
+    private void Flip() {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
     private bool SolveSystem(float[,] matrix, float[] rhs, out float a, out float b, out float c)
     {
         a = b = c = 0;
@@ -253,6 +227,13 @@ public class TrainningBossMovement : MonoBehaviour
         initialVelocity = new Vector2(v0x, v0y);
     }
 
+    private void EnableFroundProjectiles() {
+        leftProj.GetComponent<Rigidbody2D>().velocity = new Vector2(-20f, 0);
+        leftProj.Attack();
+        righttProj.GetComponent<Rigidbody2D>().velocity = new Vector2(20f, 0);
+        righttProj.Attack();
+    }
+
     private bool isGrounded() {
         RaycastHit2D groundHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, extraHeight, groundLayer | platformLayer);
         
@@ -271,9 +252,17 @@ public class TrainningBossMovement : MonoBehaviour
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col == platforms[currentPlatform].GetComponent<BoxCollider2D>()) {
-            // Debug.Log(col + "\n" + platforms[currentPlatform].GetComponent<BoxCollider2D>());
+            if (isJumpingAttack) {
+                StartCoroutine(CoroutineBeforeJumpAttack());
+            }
             EnableCollisionWithPlatforms();
         }
+    }
+
+    private IEnumerator CoroutineBeforeJumpAttack() {
+        yield return new WaitUntil( () => isGrounded() && rb.velocity.y == 0f );
+        rb.sleepMode = RigidbodySleepMode2D.StartAsleep;
+        GroundPound();
     }
 
     #endregion
@@ -287,74 +276,8 @@ public class TrainningBossMovement : MonoBehaviour
             Gizmos.DrawWireSphere(platform.transform.position, 0.5f);
         }
 
-        Gizmos.color = Color.red;
-        DrawArrow(transform.position, platforms[currentPlatform].transform.position);
-
-        Gizmos.DrawWireSphere(start, 1f);
-        Gizmos.DrawWireSphere(mid, 1f);
-        Gizmos.DrawWireSphere(end, 1f);
-
-        // Vector2 boxSize = new Vector2(groundPoundRadius * 2, 1f);
-        // Gizmos.DrawWireCube(transform.position + Vector3.down, boxSize);
-        
-    }
-
-    private void DrawArrow(Vector3 start, Vector3 end, float arrowHeadLength = 2f, float arrowHeadAngle = 90f)
-    {
-        // Рисуем основную линию стрелки
-        Gizmos.DrawLine(start, end);
-
-        // Вычисляем направление стрелки
-        Vector3 direction = (end - start).normalized;
-
-        // Рисуем правую часть наконечника стрелки
-        Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + arrowHeadAngle, 0) * Vector3.forward;
-        Gizmos.DrawLine(end, end + right * arrowHeadLength);
-
-        // Рисуем левую часть наконечника стрелки
-        Vector3 left = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 - arrowHeadAngle, 0) * Vector3.forward;
-        Gizmos.DrawLine(end, end + left * arrowHeadLength);
-    }
-
-    void DrawParabolaCurve()
-    {
-        Vector2 startPos = transform.position;
-        Vector2 endPos = platforms[currentPlatform].transform.position;
-        Vector2 midPos = new Vector2( (startPos.x + endPos.x) / 2f, (startPos.y + endPos.y) / 2f + Mathf.Abs(startPos.y - endPos.y) + 10f );
-        Vector2 point1 = startPos;
-        Vector2 point2 = endPos;
-        Vector2 point3 = midPos;
-
-        // Получаем координаты точек
-        float x1 = point1.x, y1 = point1.y;
-        float x2 = point2.x, y2 = point2.y;
-        float x3 = point3.x, y3 = point3.y;
-
-        // Решаем систему уравнений для a, b, c
-        float[,] matrix = {
-            { x1 * x1, x1, 1 },
-            { x2 * x2, x2, 1 },
-            { x3 * x3, x3, 1 }
-        };
-
-        float[] rhs = { y1, y2, y3 };
-
-        if (!SolveSystem(matrix, rhs, out float a, out float b, out float c))
-        {
-            Debug.LogError("Точки не лежат на одной параболе!");
-            return;
-        }
-
-        // Отрисовка параболы
-        float minX = Mathf.Min(x1, x2, x3);
-        float maxX = Mathf.Max(x1, x2, x3);
-
-        for (int i = 0; i < resolution; i++)
-        {
-            float x = minX + (maxX - minX) * i / (resolution - 1);
-            float y = a * x * x + b * x + c;
-            lineRenderer.SetPosition(i, new Vector3(x, y, 0));
-        }
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(attackTransform.position, meleeRange);
     }
 
     #endregion
