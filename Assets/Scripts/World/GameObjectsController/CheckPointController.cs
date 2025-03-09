67d1bbde60345f77eae1febd3390613213528bd9
Gregua1232 <grishaseleznev@yandex.ru>
2025-03-09 18:00:41 +0300

    Fix

--- Assets/Scripts/World/GameObjectsController/CheckPointController.cs
+++ Assets/Scripts/World/GameObjectsController/CheckPointController.cs
@@ -1,16 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CheckPointController : MonoBehaviour
 {
     public GameObject canvasTip;
+    public TMP_Text text;
     void Awake() {
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             canvasTip.SetActive(true);
+            text.text = "Press " + UserInput.instance.controls.GameInteraction.Interact.activeControl.path;
         }
     }
 
